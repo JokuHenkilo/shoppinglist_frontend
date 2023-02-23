a484e35cf09ff0652ca1410b4beecf3a56f6179c
JokuHenkilo <c1mais00@students.oamk.fi>
2023-02-23 11:54:24 +0200

    First part of UI done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = 'http://localhost/backend/index.php';
+
+function App()
+{
+	const [desc, setDesc] = useState('');
+	const [amount, setAmount] = useState('');
+	useEffect(() =>
+	{
+		axios.get(URL).then((response) =>
+		{
+			setDesc(response.data);
+		}).catch(error =>
+		{
+			alert(error);
+		});
+	}, []);
+	
+	function handleSubmit(e)
+	{
+		e.preventDefault();
+	}
+	
+	return (
+		<form onSubmit={handleSubmit}>
+			<div>
+				<h3>Shopping list</h3>
+			</div>
+			<div>
+				<label>New item </label>
+				<input type="text" value={desc} onChange={e => setDesc(e.target.value)}/>
+				<input type="number" value={amount} onChange={e => setAmount(e.target.value)}/>
+				<button>Add</button>
+			</div>
+		</form>
+	);
 }
 
 export default App;
